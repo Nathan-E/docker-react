222dcf65be2b4f8be796d2a86f4d0f9caa18c87f
Nathan-E <eziokwubunduchibueze.com>
2019-12-25 13:48:21 +0100

    test failing

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,7 @@ import App from './App';
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  expect(linkElement).getByText();
 });
 
 test("renders learn react link", () => {
